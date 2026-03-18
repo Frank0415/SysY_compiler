927c40d24d06efa2c1a2ba9c0a9f312a22140b57
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2026-03-18 19:33:01 +0800

    feat: 4_1 variables assign

---
+++ src/gen_ir_variables.rs
@@ -0,0 +1,55 @@
+use koopa::ir::Value;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Copy)]
+
+pub enum SymbolInfo {
+    Const(i32),
+    Var(Value),
+}
+
+pub struct Variables {
+    // Stack of scopes, where each scope is a Map
+    scopes: Vec<HashMap<String, SymbolInfo>>,
+}
+
+impl Variables {
+    pub fn new() -> Self {
+        Self { scopes: Vec::new() }
+    }
+
+    pub fn enter_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    pub fn exit_scope(&mut self) {
+        self.scopes.pop();
+    }
+
+    pub fn insert(&mut self, name: String, value: SymbolInfo) {
+        if let Some(current_scope) = self.scopes.last_mut() {
+            current_scope.insert(name, value);
+        }
+    }
+
+    pub fn get_const(&self, name: &str) -> Option<i32> {
+        // Search from most recent scope to oldest
+        self.scopes
+            .iter()
+            .rev()
+            .find_map(|scope| match scope.get(name) {
+                Some(SymbolInfo::Const(val)) => Some(*val),
+                _ => None,
+            })
+    }
+
+    pub fn get(&self, name: &str) -> Option<Value> {
+        self.scopes
+            .iter()
+            .rev()
+            .find_map(|scope| match scope.get(name) {
+                Some(SymbolInfo::Var(val)) => Some(*val),
+                _ => None,
+            })
+    }
+}
